3afb14718e91088a5296704de5c49d49b4f5a0d9
Eliyas Kassaye <eliyasson11@gmail.com>
2023-11-11 00:48:20 +0200

    play sound in js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-        alert("I got clicked!");
-
+const drum = document.querySelectorAll(".drum");
+for (let i = 0; i < drum.length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        (this.style.color = "white");
     });
-}
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-function add(num1, num2) {
-    return num1 + num2;
-}
 
-function calculator(num1, num2, operator) {    
-    return operator(num1, num2)
-}
-console.log(calculator(2, 3, multiply)); // asking the calculator to multiply 2 and 3 using the multiply function.
 
+    
+    
+}
+/*
+drum[i].addEventListener("click", () => {
+        const audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
+    })
+*/
