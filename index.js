2c451db31123f7c3cd94c30de061d84e14e35ffd
Eliyas Kassaye <eliyasson11@gmail.com>
2023-11-13 09:41:09 +0200

    using if-else instead of switch

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const drum = document.querySelectorAll(".drum");
 for (let i = 0; i < drum.length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    drum[i].addEventListener("click", function () {
         let buttonInnerHTML = this.innerHTML;
-        switch(buttonInnerHTML) {
+        if(buttonInnerHTML === "w") {
+            const tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+        } else if(buttonInnerHTML === "a"){
+            const tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+        } else if(buttonInnerHTML === "s"){
+            const tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+        } else if(buttonInnerHTML === "d"){
+            const tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+        } else if(buttonInnerHTML === "j") {
+            const snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+        } else if (buttonInnerHTML === "k") {
+            const crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+        } else if (buttonInnerHTML === "l") {
+            const kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play();
+        } else {
+
+        }
+
+
+        /*switch(buttonInnerHTML) {
             case "w": 
                 const tom1 = new Audio('./sounds/tom-1.mp3');
                 tom1.play(); 
@@ -34,8 +60,8 @@ for (let i = 0; i < drum.length; i++) {
             break;
 
             default:
-        }
-    });
+        }*/
+    }); 
  
 }
 /*
